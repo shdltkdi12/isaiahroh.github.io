261180bb70e7f4efc86451968efe8028ffc22610
Isaiah Roh <izr0007@auburn.edu>
2020-05-29 02:47:21 -0500

    added about section

--- assets/mail/submit.js
+++ assets/mail/submit.js
@@ -18,7 +18,7 @@ $(function () {
                 firstName = name.split(" ").slice(0, -1).join(" ");
             }
             $this = $("#sendMessageButton");
-            $this.prop("disabled", true);
+            $this.prop("disabled", true); // Disable submit button until AJAX call is complete to prevent duplicate messages
             var URL = "https://crdcobfj67.execute-api.us-east-1.amazonaws.com/01/contact-us"
             var data = {
                 name: name,
@@ -32,21 +32,54 @@ $(function () {
                 crossDomain: "true",
                 contentType: "application/json; charset=utf-8",
                 data: JSON.stringify(data),
-          
                 
                 success: function () {
-                  // clear form and show a success message
-                  alert("Successfull");
-                  document.getElementById("contact-form").reset();
-              location.reload();
-                },
+                  // Success message
+                  $("#success").html("<div class='alert alert-success'>");
+                  $("#success > .alert-success")
+                      .html(
+                          "<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;"
+                      )
+                      .append("</button>");
+                  $("#success > .alert-success").append(
+                      "<strong>Your message has been sent. I will respond within 1 business day. </strong>"
+                  );
+                  $("#success > .alert-success").append("</div>");
+                  //clear all fields
+                  $("#contactForm").trigger("reset");
+              },
                 error: function () {
-                  // show an error message
-                  alert("UnSuccessfull");
-                }});
+                  // Fail message
+                  $("#success").html("<div class='alert alert-danger'>");
+                  $("#success > .alert-danger")
+                      .html(
+                          "<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;"
+                      )
+                      .append("</button>");
+                  $("#success > .alert-danger").append(
+                      $("<strong>").text(
+                          "Sorry " +
+                              firstName +
+                              ", it seems that my mail server is not responding. Please try again later!"
+                      )
+                  );
+                  $("#success > .alert-danger").append("</div>");
+                  //clear all fields
+                  $("#contactForm").trigger("reset");
+              }});
                 setTimeout(function () {
                     $this.prop("disabled", false); // Re-enable submit button when AJAX call is complete
                 }, 1000);
             }
     });
+
+    $('a[data-toggle="tab"]').click(function (e) {
+        e.preventDefault();
+        $(this).tab("show");
+    });
+});
+
+/*When clicking on Full hide fail/success boxes */
+$("#name").focus(function () {
+    $("#success").html("");
 });
